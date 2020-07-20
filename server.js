8b8da3f5daf73e9eba501035c3af3aa4c21d227b
aohrekin <aokhrekin@gmail.com>
2020-07-20 19:29:42 +0300

    with server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/task3"));
 app.get('/*', function(req, res) {
-  res.sendFile('src/index.html' , { root : __dirname});
+  res.sendFile('/src/app/app.component.html' , { root : __dirname});
 });
 // root: path.join(__dirname, '../public')
 // app.get('/', function(req, res) {
